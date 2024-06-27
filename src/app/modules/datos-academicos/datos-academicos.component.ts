2d3a469cb85e43a62fd0d72520aa9d3cd160a103
Astar18 <aleister200213@gmail.com>
2024-06-27 15:29:55 -0500

    arreglar-cambios-grid

--- src/app/modules/datos-academicos/datos-academicos.component.ts
+++ src/app/modules/datos-academicos/datos-academicos.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 
 @Component({
   selector: 'app-datos-academicos',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule,MatButtonToggleModule],
   templateUrl: './datos-academicos.component.html',
   styleUrl: './datos-academicos.component.css'
 })
 export class DatosAcademicosComponent {
 
+
+  tiles: Tile[] = [
+    {text: 'One', cols: 1, rows: 4 ,color:'blue'},
+    {text: 'Two', cols: 1, rows: 4, color: ''},
+    {text: 'Three', cols: 1, rows: 4, color: ''},
+    {text: 'Four', cols: 1, rows: 4, color: ''},
+
+  ];
 }
--- src/app/modules/datos-personales/datos-personales.component.ts
+++ src/app/modules/datos-personales/datos-personales.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'app-datos-personales',
   standalone: true,
-  imports: [],
+  imports: [MatGridListModule,MatButtonToggleModule,MatInputModule,MatFormFieldModule,FormsModule,MatIconModule,MatDividerModule,MatButtonModule,MatDatepickerModule],
   templateUrl: './datos-personales.component.html',
-  styleUrl: './datos-personales.component.css'
+  styleUrl: './datos-personales.component.css',
+  providers: [provideNativeDateAdapter()]
 })
 export class DatosPersonalesComponent {
+  tiles: Tile[] = [
+    {text: 'One', cols: 1, rows: 4 ,color:'blue'},
+    {text: 'Two', cols: 1, rows: 4, color: ''},
+    {text: 'Three', cols: 1, rows: 4, color: ''},
+    {text: 'Four', cols: 1, rows: 4, color: ''},
 
+  ];
 }
