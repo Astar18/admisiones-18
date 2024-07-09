aff2c149670c7f7952fa197c7fc8f2d08cfeddbc
Astar18 <aleister200213@gmail.com>
2024-07-09 15:38:10 -0500

    cambios modulo-postulacion

--- src/app/modules/postulacion/postulacion.component.ts
+++ src/app/modules/postulacion/postulacion.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component,ChangeDetectionStrategy,signal } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatExpansionModule} from '@angular/material/expansion';
 @Component({
   selector: 'app-postulacion',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule,MatExpansionModule],
   templateUrl: './postulacion.component.html',
-  styleUrl: './postulacion.component.css'
+  styleUrl: './postulacion.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PostulacionComponent {
-
+  readonly panelOpenState = signal(false);
 }
